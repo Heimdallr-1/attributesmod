694622876e25a97abc9153bcfe187092bc4c3840
Pufferfish <puffish@puffish.net>
2024-11-09 23:18:48 +0100

    Use @WrapOperation instead of @ModifyArg

--- Common/src/main/java/net/puffish/attributesmod/mixin/LivingEntityMixin.java
+++ Common/src/main/java/net/puffish/attributesmod/mixin/LivingEntityMixin.java
@@ -1,6 +1,8 @@
 package net.puffish.attributesmod.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.attribute.EntityAttributeInstance;
@@ -12,12 +14,11 @@ import net.puffish.attributesmod.util.Sign;
 import net.puffish.attributesmod.util.Signed;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 import java.util.ArrayList;
 
-@Mixin(value = LivingEntity.class, priority = 900)
+@Mixin(value = LivingEntity.class, priority = 1100)
 public abstract class LivingEntityMixin {
 
 	@SuppressWarnings("unchecked")
@@ -55,44 +56,30 @@ public abstract class LivingEntityMixin {
 		return damage;
 	}
 
-	@ModifyArg(
+	@WrapOperation(
 			method = "applyArmorToDamage",
-			at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/DamageUtil;getDamageLeft(FFF)F"),
-			index = 1
+			at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/DamageUtil;getDamageLeft(FFF)F")
 	)
-	private float modifyArgAtApplyArmorToDamage1(float armor, @Local(argsOnly = true) DamageSource source) {
+	private float wrapOperationAtApplyArmorToDamage(float damage, float armor, float toughness, Operation<Float> operation, @Local(argsOnly = true) DamageSource source) {
 		if (source.getAttacker() instanceof PlayerEntity player) {
 			armor = Math.max(0.0f, (float) AttributesMod.applyAttributeModifiers(
 					armor,
 					Sign.NEGATIVE.wrap(player.getAttributeInstance(AttributesMod.ARMOR_SHRED))
 			));
-		}
-
-		return armor;
-	}
-
-	@ModifyArg(
-			method = "applyArmorToDamage",
-			at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/DamageUtil;getDamageLeft(FFF)F"),
-			index = 2
-	)
-	private float modifyArgAtApplyArmorToDamage2(float toughness, @Local(argsOnly = true) DamageSource source) {
-		if (source.getAttacker() instanceof PlayerEntity player) {
 			toughness = Math.max(0.0f, (float) AttributesMod.applyAttributeModifiers(
 					toughness,
 					Sign.NEGATIVE.wrap(player.getAttributeInstance(AttributesMod.TOUGHNESS_SHRED))
 			));
 		}
 
-		return toughness;
+		return operation.call(damage, armor, toughness);
 	}
 
-	@ModifyArg(
+	@WrapOperation(
 			method = "modifyAppliedDamage",
-			at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/DamageUtil;getInflictedDamage(FF)F"),
-			index = 1
+			at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/DamageUtil;getInflictedDamage(FF)F")
 	)
-	private float modifyArgAtModifyAppliedDamage(float protection, @Local(argsOnly = true) DamageSource source) {
+	private float wrapOperationAtModifyAppliedDamage(float damageDealt, float protection, Operation<Float> original, @Local(argsOnly = true) DamageSource source) {
 		if (source.getAttacker() instanceof PlayerEntity player) {
 			protection = Math.max(0.0f, (float) AttributesMod.applyAttributeModifiers(
 					protection,
@@ -100,7 +87,7 @@ public abstract class LivingEntityMixin {
 			));
 		}
 
-		return protection;
+		return original.call(damageDealt, protection);
 	}
 
 	@ModifyVariable(
